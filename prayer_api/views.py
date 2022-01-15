7b541c05fb496ab4da9a09b603b49bbfc6da4092
Abdullah Awad <abdullah.alashafi@gmail.com>
2022-01-15 23:05:51 +0300

    use requests get api 1st

--- prayer_api/views.py
+++ prayer_api/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
+
 def home(request):
     print(request)
+    result = requests.get("http://api.aladhan.com/v1/timingsByCity?method=8&city=Makkah&country=Saudi%20Arabia")
     return HttpResponse("Good Work, Keep on it💪🏼")
