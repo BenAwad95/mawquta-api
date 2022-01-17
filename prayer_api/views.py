0b7c35044c2c862795f342f31868785d29135c70
Abdullah Awad <abdullah.alashafi@gmail.com>
2022-01-17 23:38:04 +0300

    represent prayers timings in html format

--- prayer_api/views.py
+++ prayer_api/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 import requests
+import json
 
 def home(request):
-    result = requests.get("http://api.aladhan.com/v1/timingsByCity?method=8&city=Makkah&country=Saudi%20Arabia")
+    city = 'Jeddah'
+    country = 'Saudi Arabia'
+    method = 4
+    result = requests.get(f"http://api.aladhan.com/v1/timingsByCity?method={method}&city={city}&country={country}")
     data = result.json()
+    print(type(data))
+    # data = json.dumps(data)
+    # print(type(data))
+    data = data['data']
     # return HttpResponse(data)
-    return JsonResponse(data)
+    # return JsonResponse(data)
+    return render(request, 'prayer/city_timing.html', context={'city': city, 'hijri': data['date']['hijri'], 'gregorian': data['date']['gregorian'], 'timings': data['timings']})
